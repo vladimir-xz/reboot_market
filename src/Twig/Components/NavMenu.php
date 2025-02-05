dac1a000d25fd3864c14df33451642ca18dff23d
Vladimir <vladimir156xz@gmail.com>
2025-02-05 21:16:40 +0100

    dynamic nav menu

--- src/Twig/Components/NavMenu.php
+++ src/Twig/Components/NavMenu.php
@@ -7,4 +7,9 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent]
 final class NavMenu
 {
+    public array $categories;
+    public array $leftPaddings = [
+        'pl-6', 'pl-9', 'pl-12', 'pl-15', 'pl-18'
+    ];
+    public int $offset = 0;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "./assets/**/*.js",
     "./templates/**/*.html.twig",
   ],
+  safelist: [
+    'pl-6', 'pl-9', 'pl-12', 'pl-15', 'pl-18'
+  ],
   theme: {
     extend: {},
   },
