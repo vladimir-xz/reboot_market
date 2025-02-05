3be8b006129511763738b8a94448ced5744c0c36
Vladimir <vladimir156xz@gmail.com>
2025-02-05 21:21:23 +0100

    deleted redundant property in NavMenu

--- src/Twig/Components/NavMenu.php
+++ src/Twig/Components/NavMenu.php
@@ -8,8 +8,5 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 final class NavMenu
 {
     public array $categories;
-    public array $leftPaddings = [
-        'pl-6', 'pl-9', 'pl-12', 'pl-15', 'pl-18'
-    ];
-    public int $offset = 0;
+    public int $offset = 6;
 }
