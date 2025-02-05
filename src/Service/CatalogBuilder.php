82c1b310617c19b4c0d36205a08266aff2a7e9c3
Vladimir <vladimir156xz@gmail.com>
2025-02-05 22:48:42 +0100

    create additional tree for parents

--- src/Service/CatalogBuilder.php
+++ src/Service/CatalogBuilder.php
@@ -6,24 +6,26 @@ class CatalogBuilder
 {
     public function build(array $rawCatalog): array
     {
-        $tree = [];
+        $treeForChildren = [];
+        $treeForParents = [];
         $mainNode = [];
         foreach ($rawCatalog as $index => $data) {
             $parentId = $data['parent_id'];
             if ($parentId === null) {
                 $mainNode[] = $index;
             } else {
-                $tree[$parentId][] = $index;
+                $treeForChildren[$parentId][] = $index;
+                $treeForParents[$index] = $parentId;
             }
         }
 
-        $buildCatalog = function ($array) use ($rawCatalog, $tree, &$buildCatalog) {
-            return array_map(function ($index) use ($rawCatalog, $tree, $buildCatalog) {
+        $buildCatalog = function ($array) use ($rawCatalog, $treeForChildren, &$buildCatalog) {
+            return array_map(function ($index) use ($rawCatalog, $treeForChildren, $buildCatalog) {
                 $result = $rawCatalog[$index];
-                $result['index'] = $index;
+                $result['id'] = $index;
 
-                if (array_key_exists($index, $tree)) {
-                    $result['children'] = $buildCatalog($tree[$index]);
+                if (array_key_exists($index, $treeForChildren)) {
+                    $result['children'] = $buildCatalog($treeForChildren[$index]);
                 }
 
                 return $result;
