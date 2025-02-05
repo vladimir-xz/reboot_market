1edee626141d0a04b023c3ddac073f1397ea9fd0
Vladimir <vladimir156xz@gmail.com>
2025-02-05 18:56:53 +0100

    working nested nav

---
+++ assets/controllers/catalog_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from '@hotwired/stimulus';
+import { getComponent } from '@symfony/ux-live-component';
+
+/*
+* The following line makes this controller "lazy": it won't be downloaded until needed
+* See https://github.com/symfony/stimulus-bridge#lazy-controllers
+*/
+/* stimulusFetch: 'lazy' */
+export default class extends Controller {
+    static targets = ['node']
+
+    async initialize() {
+        this.component = await getComponent(this.element);
+    }
+
+    onClick(event) {
+        const nextElement = event.target.nextElementSibling
+        if (nextElement && nextElement.tagName === 'DIV') {
+            nextElement.classList.toggle("hidden")
+        }
+    }
+}
