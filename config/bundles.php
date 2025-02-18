a43f3e424050c4ca8301f8fe853b671ec20eff12
vladimir-xz <vladimir156xz@gmail.com>
2025-02-18 21:05:52 +0100

    changed config/bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -17,5 +17,5 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
 ];
