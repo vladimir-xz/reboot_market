8eca0ed627d615cedba97ba59eba248888406cba
vladimir-xz <vladimir156xz@gmail.com>
2025-02-21 10:30:24 +0100

    preparing to return hardcoded tree for better performance

--- src/Service/CatalogBuilder.php
+++ src/Service/CatalogBuilder.php
@@ -12,41 +12,50 @@ class CatalogBuilder
     }
     public function build(array $rawCatalog): array
     {
-        $treeOfChildren = [];
-        $treeOfParents = [];
-        $mainNode = [];
-        foreach ($rawCatalog as $index => $data) {
-            $parentId = $data['parent_id'];
-            if ($parentId === null) {
-                $mainNode[$index] = $index;
-            } else {
-                $treeOfChildren[$parentId][] = $index;
-                $treeOfParents[$index] = $parentId;
-            }
-        }
-
-        $lastNodesAndParents = $this->mapLastNodesAndParents($mainNode, $treeOfChildren);
-
-        $buildCatalog = function ($array) use ($rawCatalog, $treeOfChildren, &$buildCatalog) {
-            return array_map(function ($index) use ($rawCatalog, $treeOfChildren, $buildCatalog) {
-                $result = $rawCatalog[$index];
-                $result['id'] = $index;
-
-                if (array_key_exists($index, $treeOfChildren)) {
-                    $result['children'] = $buildCatalog($treeOfChildren[$index]);
-                }
-
-                return $result;
-            }, $array);
-        };
-
-        $treeToDisplay = $buildCatalog($mainNode);
+        $tree = '{"1":{"parent_id":null,"name":"Servers","id":1,"children":[{"parent_id":1,"name":"Dell","id":6,"children":[{"parent_id":6,"name":"2.5 FormFactor","id":17},{"parent_id":6,"name":"3.5 FormFactor","id":18}]},{"parent_id":1,"name":"HP","id":7,"children":[{"parent_id":7,"name":"2.5 FormFactor","id":19},{"parent_id":7,"name":"3.5 FormFactor","id":20}]}]},"2":{"parent_id":null,"name":"Storage","id":2,"children":[{"parent_id":2,"name":"Dell","id":8},{"parent_id":2,"name":"HP","id":9}]},"3":{"parent_id":null,"name":"Network Equipment","id":3,"children":[{"parent_id":3,"name":"Switches","id":10}]},"4":{"parent_id":null,"name":"Components","id":4,"children":[{"parent_id":4,"name":"RAM","id":11},{"parent_id":4,"name":"CPUs (Processors)","id":12},{"parent_id":4,"name":"Drives","id":13},{"parent_id":4,"name":"Network cards","id":14},{"parent_id":4,"name":"Power supplies","id":15}]},"5":{"parent_id":null,"name":"Others","id":5,"children":[{"parent_id":5,"name":"Racks","id":16}]}}';
+        $parents = '{"6":1,"7":1,"8":2,"9":2,"10":3,"11":4,"12":4,"13":4,"14":4,"15":4,"16":5,"17":6,"18":6,"19":7,"20":7}';
+        $lastCHildren = '{"6":{"17":17,"18":18},"1":{"17":17,"18":18,"19":19,"20":20},"7":{"19":19,"20":20},"2":{"8":8,"9":9},"3":{"10":10},"4":{"11":11,"12":12,"13":13,"14":14,"15":15},"5":{"16":16}}';
 
         return [
-            'parents' => $treeOfParents,
-            'lastChildren' => $lastNodesAndParents['lastChildren'],
-            'catalog' => $treeToDisplay
+            'catalog' => $tree,
+            'parents' => $parents,
+            'lastChildren' => $lastCHildren,
         ];
+        // $treeOfChildren = [];
+        // $treeOfParents = [];
+        // $mainNode = [];
+        // foreach ($rawCatalog as $index => $data) {
+        //     $parentId = $data['parent_id'];
+        //     if ($parentId === null) {
+        //         $mainNode[$index] = $index;
+        //     } else {
+        //         $treeOfChildren[$parentId][] = $index;
+        //         $treeOfParents[$index] = $parentId;
+        //     }
+        // }
+
+        // $lastNodesAndParents = $this->mapLastNodesAndParents($mainNode, $treeOfChildren);
+
+        // $buildCatalog = function ($array) use ($rawCatalog, $treeOfChildren, &$buildCatalog) {
+        //     return array_map(function ($index) use ($rawCatalog, $treeOfChildren, $buildCatalog) {
+        //         $result = $rawCatalog[$index];
+        //         $result['id'] = $index;
+
+        //         if (array_key_exists($index, $treeOfChildren)) {
+        //             $result['children'] = $buildCatalog($treeOfChildren[$index]);
+        //         }
+
+        //         return $result;
+        //     }, $array);
+        // };
+
+        // $treeToDisplay = $buildCatalog($mainNode);
+
+        // return [
+        //     'parents' => $treeOfParents,
+        //     'lastChildren' => $lastNodesAndParents['lastChildren'],
+        //     'catalog' => $treeToDisplay
+        // ];
     }
 
     private function mapLastNodesAndParents(
