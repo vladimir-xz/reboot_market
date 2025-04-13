640a37f4bec5386892757650066c1ae3b2cb9af0
Vladimir <vladimir156xz@gmail.com>
2025-04-13 15:58:07 +0200

    design improvement

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,24 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'white-granular': "url('/images/background/white.jpg')",
+        'grey-granular': "url('/images/background/grey.jpg')",
+        'black-granular': "url('/images/background/drk.jpg')",
+        'pitch-black': "url('/images/background/pitch.jpg')",
+      },
+      backgroundSize: {
+        '50': '50px'
+      },
+      colors: {
+        'amber-dim': '#e19e2f',
+        'orange-yellow': '#F9DDAA',
+        'green-dark': '#11333c',
+        'dark-charcoal': '#2B2B2B',
+        'white-soft': '#fdfaf7',
+        'grey-dark': '#2a2a29',
+
+      },
       fontFamily: {
         roboto: ['Roboto', 'sans-serif'],
       },
